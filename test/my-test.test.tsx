f5f8caf01308891c637b67139888872d53feb4cd
yamachu <yamachu.dev@gmail.com>
2023-10-26 20:06:41 +0900

    Add test

---
+++ test/my-test.test.tsx
@@ -0,0 +1,17 @@
+import { test, expect } from "vitest";
+
+test("test", () => {
+  let assertCount = 0;
+  const myEventType = "my-event" as const;
+
+  const element = document.createElement("div");
+  element.addEventListener(myEventType, () => {
+    assertCount++;
+  });
+
+  expect(() =>
+    element.dispatchEvent(new CustomEvent(myEventType))
+  ).not.toThrowError();
+
+  expect(assertCount).toBe(1);
+});
